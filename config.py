361b5777a2baf4dd50cd840b1ad6f67bbabadb12
mmnagwa <nagwammatia919@gmail.com>
2025-10-28 18:27:43 +0300

    Update config.py

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ if not google_api_key:
     raise ValueError("GOOGLE_API_KEY is missing. Please add it to Streamlit Secrets.")
 
 llm = LLM(
-    provider="google",
     model="gemini/gemini-2.5-flash",  
     api_key=google_api_key,
     temperature=0.7
 )
+
