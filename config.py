2205f7a31f11b4250d6bbc65cc9dfe7a9807fba8
mmnagwa <nagwammatia919@gmail.com>
2025-10-28 11:53:15 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,16 @@
 import os
 import google.generativeai as genai
 from crewai import LLM
-gemini_api_key = os.getenv("GEMINI_API_KEY")
-genai.configure(api_key=gemini_api_key)
+google_api_key = os.getenv("GOOGLE_API_KEY")
+genai.configure(api_key=google_api_key)
 
 
 llm = LLM(
-    model="gemini-1.5-flash",
+    model="gemini-1.5-flash",  
     api_key=os.getenv("GOOGLE_API_KEY"),
     temperature=0.7
 )
+
 
 
 
